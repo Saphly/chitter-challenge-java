c66e705403171c23a27f0cb7b6c143a8eb9aee73
KW <kee.wei26@gmail.com>
2024-03-05 18:08:39 +0000

    reformat peep controller tests

--- src/test/java/com/chitterchallenge/server/PeepControllerTests.java
+++ src/test/java/com/chitterchallenge/server/PeepControllerTests.java
@@ -40,7 +40,7 @@ class PeepControllerTests {
 
 	@Nested
 	@DisplayName("GET peeps tests")
-	class GetAll {
+	class GetAllPeepsTests {
 		@Test
 		@DisplayName("Should return OK Http Status code - regardless of how many peeps are found")
 		void shouldReturnOkHttpStatusCode() throws Exception {
@@ -53,46 +53,42 @@ class PeepControllerTests {
 			mockMvc.perform(get("/peep/all"))
 					.andExpect(content().contentType(MediaType.APPLICATION_JSON));
 		}
-	}
-
-	@Nested
-	@DisplayName("When 1 or more peeps are found")
-	class WhenPeepsFound {
-		@BeforeEach
-		void repopulateCollection() {
-			TestMongoConfig.repopulatePeepsCollection(peeps);
-		}
 
 		@Test
-		@DisplayName("Should return a list with the correct length")
-		void shouldReturnListOfRightLength() throws Exception {
-			mockMvc.perform(get("/peep/all"))
-					.andExpect(jsonPath("$", hasSize(peeps.size())));
-		}
-
-		@Test
-		@DisplayName("Should return list in reverse chronological order")
-		void shouldReturnListInReverseChronologicalOrder() throws Exception {
-			mockMvc
-					.perform(get("/peep/all"))
-					.andDo(print())
-					.andExpect(jsonPath("$[*].dateCreated", contains("2024-03-04T22:04:28.674Z", "2024-03-04T22:04:00.675Z")));
-		}
-	}
-
-	@Nested
-	@DisplayName("When peeps collection is empty")
-	class NoPeepsFound {
-		@Test
-		@DisplayName("Should return a ArrayList with size 0")
+		@DisplayName("Should return a ArrayList with size 0 when there are no peeps")
 		void shouldReturnArrayListWithSize0() throws Exception {
 			mockMvc.perform(get("/peep/all"))
 					.andExpect(jsonPath("$", hasSize(0)));
 		}
+
+		@Nested
+		@DisplayName("When 1 or more peeps are found")
+		class WhenPeepsFound {
+			@BeforeEach
+			void repopulateCollection() {
+				TestMongoConfig.repopulatePeepsCollection(peeps);
+			}
+
+			@Test
+			@DisplayName("Should return a list with the correct length")
+			void shouldReturnListOfRightLength() throws Exception {
+				mockMvc.perform(get("/peep/all"))
+						.andExpect(jsonPath("$", hasSize(peeps.size())));
+			}
+
+			@Test
+			@DisplayName("Should return list in reverse chronological order")
+			void shouldReturnListInReverseChronologicalOrder() throws Exception {
+				mockMvc
+						.perform(get("/peep/all"))
+						.andDo(print())
+						.andExpect(jsonPath("$[*].dateCreated", contains("2024-03-04T22:04:28.674Z", "2024-03-04T22:04:00.675Z")));
+			}
+		}
 	}
 
 	@Nested
-	@DisplayName("Integrated POST Request Tests")
+	@DisplayName("POST Peep Tests")
 	class PostPeepRequestTests {
 		@Nested
 		@DisplayName("New valid peep tests")
@@ -130,70 +126,70 @@ class PeepControllerTests {
 				);
 			}
 		}
-	}
 
-	@Nested
-	@DisplayName("Invalid POST peep request tests")
-	class InvalidPostPeepRequestTests {
+		@Nested
+		@DisplayName("Invalid POST peep request tests")
+		class InvalidPostPeepRequestTests {
 
-		@Test
-		@DisplayName("Should return status code 400 when no username is provided")
-		void shouldReturnStatusCode400WhenNoUsername() throws Exception {
-			requestBody = "{\"name\": \"" + peeps.get(0).getName() +
-					"\",\"dateCreated\": \"" + peeps.get(0).getDateCreated() + "Z" +
-					"\",\"peep\": \"" + peeps.get(0).getPeep() +
-					"\"}";
+			@Test
+			@DisplayName("Should return status code 400 when no username is provided")
+			void shouldReturnStatusCode400WhenNoUsername() throws Exception {
+				requestBody = "{\"name\": \"" + peeps.get(0).getName() +
+						"\",\"dateCreated\": \"" + peeps.get(0).getDateCreated() + "Z" +
+						"\",\"peep\": \"" + peeps.get(0).getPeep() +
+						"\"}";
 
-			mockMvc.perform(
-					post("/peep/post")
-							.content(requestBody)
-							.contentType(MediaType.APPLICATION_JSON)
-			).andExpect(status().isBadRequest());
-		}
+				mockMvc.perform(
+						post("/peep/post")
+								.content(requestBody)
+								.contentType(MediaType.APPLICATION_JSON)
+				).andExpect(status().isBadRequest());
+			}
 
-		@Test
-		@DisplayName("Should return status code 400 when no name is provided")
-		void shouldReturnStatusCode400WhenNoName() throws Exception {
-			requestBody = "{\"username\": \"" + peeps.get(0).getUsername() +
-					"\",\"dateCreated\": \"" + peeps.get(0).getDateCreated() + "Z" +
-					"\",\"peep\": \"" + peeps.get(0).getPeep() +
-					"\"}";
+			@Test
+			@DisplayName("Should return status code 400 when no name is provided")
+			void shouldReturnStatusCode400WhenNoName() throws Exception {
+				requestBody = "{\"username\": \"" + peeps.get(0).getUsername() +
+						"\",\"dateCreated\": \"" + peeps.get(0).getDateCreated() + "Z" +
+						"\",\"peep\": \"" + peeps.get(0).getPeep() +
+						"\"}";
 
-			mockMvc.perform(
-					post("/peep/post")
-							.content(requestBody)
-							.contentType(MediaType.APPLICATION_JSON)
-			).andExpect(status().isBadRequest());
-		}
+				mockMvc.perform(
+						post("/peep/post")
+								.content(requestBody)
+								.contentType(MediaType.APPLICATION_JSON)
+				).andExpect(status().isBadRequest());
+			}
 
-		@Test
-		@DisplayName("Should return status code 400 when no dateCreated is provided")
-		void shouldReturnStatusCode400WhenNoDateCreated() throws Exception {
-			requestBody = "{\"username\": \"" + peeps.get(0).getUsername() +
-					"\", \"name\": \"" + peeps.get(0).getName() +
-					"\",\"peep\": \"" + peeps.get(0).getPeep() +
-					"\"}";
+			@Test
+			@DisplayName("Should return status code 400 when no dateCreated is provided")
+			void shouldReturnStatusCode400WhenNoDateCreated() throws Exception {
+				requestBody = "{\"username\": \"" + peeps.get(0).getUsername() +
+						"\", \"name\": \"" + peeps.get(0).getName() +
+						"\",\"peep\": \"" + peeps.get(0).getPeep() +
+						"\"}";
 
-			mockMvc.perform(
-					post("/peep/post")
-							.content(requestBody)
-							.contentType(MediaType.APPLICATION_JSON)
-			).andExpect(status().isBadRequest());
-		}
+				mockMvc.perform(
+						post("/peep/post")
+								.content(requestBody)
+								.contentType(MediaType.APPLICATION_JSON)
+				).andExpect(status().isBadRequest());
+			}
 
-		@Test
-		@DisplayName("Should return status code 400 when no peep is provided")
-		void shouldReturnStatusCode400WhenNoPeep() throws Exception {
-			requestBody = "{\"username\": \"" + peeps.get(0).getUsername() +
-					"\", \"name\": \"" + peeps.get(0).getName() +
-					"\",\"dateCreated\": \"" + peeps.get(0).getDateCreated() + "Z" +
-					"\"}";
+			@Test
+			@DisplayName("Should return status code 400 when no peep is provided")
+			void shouldReturnStatusCode400WhenNoPeep() throws Exception {
+				requestBody = "{\"username\": \"" + peeps.get(0).getUsername() +
+						"\", \"name\": \"" + peeps.get(0).getName() +
+						"\",\"dateCreated\": \"" + peeps.get(0).getDateCreated() + "Z" +
+						"\"}";
 
-			mockMvc.perform(
-					post("/peep/post")
-							.content(requestBody)
-							.contentType(MediaType.APPLICATION_JSON)
-			).andExpect(status().isBadRequest());
+				mockMvc.perform(
+						post("/peep/post")
+								.content(requestBody)
+								.contentType(MediaType.APPLICATION_JSON)
+				).andExpect(status().isBadRequest());
+			}
 		}
 	}
 }
